bb2933068196fb0ee716082173c42d2bf5444b43
f3rmunoz21 <f3rmunoz21@gmail.com>
2021-03-10 15:00:54 -0700

    more added comentary, fixed bugs, exit feature.

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -30,16 +30,26 @@ export class QuestionsComponent implements OnInit {
   }
   GetQuestions(type: string): void {
     this.questionsService.GetQuestions(type);
+    // Each button that is displayed for the tests available to take passes in the type parameter and runs 
+    // the GetQuestions method on the questionsService.
   }
+
   Restart(): void {
     if (this.type === undefined) {
       this.questionsService.GetQuestions(this.type);
       this.questionsService.GetMixAndMatch(this.type);
+      // If the question type for the quiz is undefined, the 
+      // type will be equal to the the current type of test being taken.
     } else {
       this.questionsService.GetQuestions(this.type);
       this.questionsService.GetMixAndMatch(this.type);
-      console.log("restart is working");
+      // If the question type is already defined then the quiz will 
+      // restart using the type to get the questions from firebase
     }
+    this.questionsService.answeredQuestions.splice(0, this.questionsService.answeredQuestions.length);
+    console.log(this.questionsService.answeredQuestions);
+    // The answeredQuestions array needs to be reset to zero, if the array is not reset to zero 
+    // The results will display all the results for the amount of tests/quizes taken.
   }
 
   submit() { 
@@ -47,6 +57,9 @@ export class QuestionsComponent implements OnInit {
       this.fillInTheBlankIsAnsweredTF = true;
       this.questionsService.GetResult(this.typedAnswer);
       this.questionsService.currentQuestion.isAnsweredTF = true;
+      // If a typed answer exists then the answered booleans will be set to true and will 
+      // run the GetResult method using the user's typed answer as its paramater.
+
     } else { 
       console.log("No answer was recorded");
       this.fillInTheBlankIsAnsweredTF = false;
@@ -59,6 +72,7 @@ export class QuestionsComponent implements OnInit {
     this.currentQuestion = question; 
     this.sendAnswers();
     console.log("questions is running");
+    // runs sendAnswers method.
   }
 
   setCurrentAnswer(answer: string, index: number) { 
@@ -66,6 +80,7 @@ export class QuestionsComponent implements OnInit {
     this.currentAnswer = answer;
     this.sendAnswers();
     console.log("answers is running");
+    // runs send answers method
   }
 
   sendAnswers() { 
@@ -74,6 +89,8 @@ export class QuestionsComponent implements OnInit {
       this.questionsService.getResultMixAndMatch(this.currentQuestion, this.currentAnswer, this.index);
       this.currentQuestion = null;
       this.currentAnswer = null;
+      // If currentAnswer and currentQUestion are both not null, then the mixAndMatchQuestionWasAnswerd and the getResultMixAndMatch
+      // methods will run with their corresponding paramater values being passed through.
     }  
   }
 
@@ -82,12 +99,9 @@ export class QuestionsComponent implements OnInit {
     this.questionsService.GetQuestions(this.type);
     this.questionsService.GetMixAndMatch(this.type);
     // The question type was set by the buttons in questions.components.html when the user selects the 
-    // type of test/quiz they wish to take
+    // type of test/quiz they wish to take.
     this.questionsService.dropDownTestsTF = false;
     // setting the dropDownTestsTF to false will deactivate the selection menu for the tests available 
   }
 
-  quit() { 
-    this.questionsService.dropDownTestsTF = true;
-  }
 }
